bf72fabf1393376e47b7ef07b9f6b2be1d38e5fa
engendromestre <engendro.mestre@gmail.com>
2023-05-10 23:33:51 -0300

    first commit

---
+++ Client-Server-API/server/go.mod
@@ -0,0 +1,14 @@
+module github.com/engendromestre/curso-go/challenge/CLient-Server-API
+
+go 1.20
+
+require (
+	gorm.io/driver/sqlite v1.5.0
+	gorm.io/gorm v1.24.7-0.20230306060331-85eaf9eeda11
+)
+
+require (
+	github.com/jinzhu/inflection v1.0.0 // indirect
+	github.com/jinzhu/now v1.1.5 // indirect
+	github.com/mattn/go-sqlite3 v1.14.16 // indirect
+)
---
+++ Client-Server-API/server/main.go
@@ -0,0 +1,83 @@
+package main
+
+import (
+	"encoding/json"
+	"gorm.io/driver/sqlite"
+	"gorm.io/gorm"
+	"io/ioutil"
+	"log"
+	"net/http"
+	"time"
+)
+
+type Exchange struct {
+	ID     int `gorm:"primaryKey"`
+	USDBRL struct {
+		Code        string `json:"code"`
+		Codein      string `json:"codein"`
+		Name        string `json:"name"`
+		High        string `json:"high"`
+		Low         string `json:"low"`
+		VarBid      string `json:"varBid"`
+		PctChange   string `json:"pctChange"`
+		Bid         string `json:"bid"`
+		Ask         string `json:"ask"`
+		Timestamp   string `json:"timestamp"`
+		Create_date string `json:"create_date"`
+	}
+}
+
+func main() {
+	http.HandleFunc("/cotacao", SearchExchangeHandler)
+	http.ListenAndServe(":8080", nil)
+}
+
+func SearchExchangeHandler(w http.ResponseWriter, r *http.Request) {
+	if r.URL.Path != "/cotacao" {
+		w.WriteHeader(http.StatusNotFound)
+		return
+	}
+
+	ctx := r.Context()
+	log.Println("Request Initiate")
+	defer log.Println("Request Finished")
+	select {
+	case <-time.After(200 * time.Millisecond):
+		// print in command line of the server (stdout)
+		log.Println("Request successfully processed")
+		// print in browser
+		exchange, error := SearchExchange()
+		if error != nil {
+			w.WriteHeader(http.StatusInternalServerError)
+			return
+		}
+		w.Header().Set("Content-Type", "application/json")
+		w.WriteHeader(http.StatusOK)
+		w.Write([]byte(exchange.USDBRL.Bid))
+		select {
+		case <-time.After(10 * time.Millisecond):
+			json.NewEncoder(w).Encode(exchange)
+		}
+	case <-ctx.Done():
+		log.Println("Request Cancelled by Client")
+	}
+}
+
+func SearchExchange() (*Exchange, error) {
+	resp, error := http.Get("https://economia.awesomeapi.com.br/json/last/USD-BRL")
+	if error != nil {
+		return nil, error
+	}
+	defer resp.Body.Close()
+	body, error := ioutil.ReadAll(resp.Body)
+	if error != nil {
+		return nil, error
+	}
+
+	var e Exchange
+	error = json.Unmarshal(body, &e)
+	if error != nil {
+		return nil, error
+	}
+	return &e, nil
+}
